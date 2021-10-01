a2d722b566772479c2e249775e13adf8e334e436
DerickLFAndrade <derickspy15@gmail.com>
2021-10-01 05:21:50 -0300

    Atualização design e Criação das funções

--- Js.js
+++ Js.js
@@ -1,9 +1,10 @@
 var card1 = document.getElementById("card1")
 var card2 = document.getElementById("card2")
 var card3 = document.getElementById("card3")
-card1.addEventListener('click', mudaCor1)
-card2.addEventListener('click', mudaCor2)
-card3.addEventListener('click', mudaCor3)
+var botao = document.getElementById('botao')
+var botao2 = document.getElementById('switch-shadow')
+
+
 function mudaCor1() {
     var plano = document.getElementById('pl')
     var rs = document.getElementById('rs')
@@ -14,41 +15,6 @@ function mudaCor1() {
     var lista3 = document.getElementById('lista3')
     var link = document.getElementById('link')
     var linka = document.getElementById('linka')
-   
-    lista1.style.borderColor = 'rgb(235, 228, 228)'
-    lista2.style.borderColor = 'rgb(235, 228, 228)'
-    grupoL.style.borderColor = 'rgb(235, 228, 228)'
-
-    lista1.style.color = 'white'
-    lista2.style.color = 'white'
-    lista3.style.color = 'white'
-
-    lista1.style.background = 'hsl(236, 72%, 79%)'
-    lista2.style.background = 'hsl(236, 72%, 79%)'
-    lista3.style.background = 'hsl(236, 72%, 79%)'
-
-    lista1.style.fontWeight = 'unset'
-    lista2.style.fontWeight = 'unset'
-    lista3.style.fontWeight = 'unset'
-
-    preços.style.color = 'white'
-    rs.style.color = 'white'
-    plano.style.color = 'white'
-
-    card1.style.background = 'hsl(236, 72%, 79%)'
-    link.style.background = 'white'
-    linka.style.color = 'hsl(236, 72%, 79%)'
-  }
-function mudaCor2() {
-    var plano = document.getElementById('pl')
-    var rs = document.getElementById('rs')
-    var preços = document.getElementById('preços')
-    var grupoL = document.getElementById('grupoLista1')
-    var lista1 = document.getElementById('lista4')
-    var lista2 = document.getElementById('lista5')
-    var lista3 = document.getElementById('lista6')
-    var link = document.getElementById('link')
-    var linka = document.getElementById('linka')
 
     lista1.style.borderColor = 'rgb(235, 228, 228)'
     lista2.style.borderColor = 'rgb(235, 228, 228)'
@@ -67,14 +33,99 @@ function mudaCor2() {
     lista3.style.fontWeight = 'unset'
 
     preços.style.color = 'white'
+    preços.style.fontSize = '50pt'
     rs.style.color = 'white'
+   // rs.style.left = '0px'
     plano.style.color = 'white'
-
+    plano.style.fontSize = '20pt'
     card1.style.background = 'hsl(236, 72%, 79%)'
+
     link.style.background = 'white'
     linka.style.color = 'hsl(236, 72%, 79%)'
 }
+function mudaCor2() {
+
+
+    var plano = document.getElementById('pl2')
+    var rs = document.getElementById('rs2')
+    var preços = document.getElementById('preços2')
+    var grupoL = document.getElementById('grupoLista2')
+    var lista1 = document.getElementById('lista4')
+    var lista2 = document.getElementById('lista5')
+    var lista3 = document.getElementById('lista6')
+    var link = document.getElementById('link2')
+    var linka = document.getElementById('linka2')
+
+    lista1.style.borderColor = 'rgb(235, 228, 228)'
+    lista2.style.borderColor = 'rgb(235, 228, 228)'
+    grupoL.style.borderColor = 'rgb(235, 228, 228)'
+
+    lista1.style.color = 'white'
+    lista2.style.color = 'white'
+    lista3.style.color = 'white'
+
+    lista1.style.background = 'hsl(236, 72%, 79%)'
+    lista2.style.background = 'hsl(236, 72%, 79%)'
+    lista3.style.background = 'hsl(236, 72%, 79%)'
+
+    lista1.style.fontWeight = 'unset'
+    lista2.style.fontWeight = 'unset'
+    lista3.style.fontWeight = 'unset'
+
+    preços.style.color = 'white'
+    preços.style.fontSize = '50pt'
+    rs.style.color = 'white'
+   // rs.style.left = '0px'
+
+    plano.style.color = 'white'
+    plano.style.fontSize = '20pt'
+
+    card2.style.background = 'hsl(236, 72%, 79%)'
+    link.style.background = 'white'
+    linka.style.color = 'hsl(236, 72%, 79%)'
+
+}
 function mudaCor3() {
+    var plano = document.getElementById('pl3')
+    var rs = document.getElementById('rs3')
+    var preços = document.getElementById('preços3')
+    var grupoL = document.getElementById('grupoLista3')
+    var lista1 = document.getElementById('lista7')
+    var lista2 = document.getElementById('lista8')
+    var lista3 = document.getElementById('lista9')
+    var link = document.getElementById('link3')
+    var linka = document.getElementById('linka3')
+
+    lista1.style.borderColor = 'rgb(235, 228, 228)'
+    lista2.style.borderColor = 'rgb(235, 228, 228)'
+    grupoL.style.borderColor = 'rgb(235, 228, 228)'
+
+    lista1.style.color = 'white'
+    lista2.style.color = 'white'
+    lista3.style.color = 'white'
+
+    lista1.style.background = 'hsl(236, 72%, 79%)'
+    lista2.style.background = 'hsl(236, 72%, 79%)'
+    lista3.style.background = 'hsl(236, 72%, 79%)'
+
+    lista1.style.fontWeight = 'unset'
+    lista2.style.fontWeight = 'unset'
+    lista3.style.fontWeight = 'unset'
+
+    preços.style.color = 'white'
+    preços.style.fontSize = '50pt'
+    rs.style.color = 'white'
+    //rs.style.left = '0px'
+
+    plano.style.color = 'white'
+    plano.style.fontSize = '20pt'
+
+    card3.style.background = 'hsl(236, 72%, 79%)'
+    link.style.background = 'white'
+    linka.style.color = 'hsl(236, 72%, 79%)'
+}
+
+function voltaCor1() { 
     var plano = document.getElementById('pl')
     var rs = document.getElementById('rs')
     var preços = document.getElementById('preços')
@@ -85,27 +136,148 @@ function mudaCor3() {
     var link = document.getElementById('link')
     var linka = document.getElementById('linka')
 
-    lista1.style.borderColor = 'rgb(235, 228, 228)'
-    lista2.style.borderColor = 'rgb(235, 228, 228)'
-    grupoL.style.borderColor = 'rgb(235, 228, 228)'
+    lista1.style.borderColor = 'gray'
+    lista2.style.borderColor = 'gray'
+    grupoL.style.borderColor = 'gray'
 
-    lista1.style.color = 'white'
-    lista2.style.color = 'white'
-    lista3.style.color = 'white'
+    lista1.style.color = 'hsl(229, 10%, 57%)'
+    lista2.style.color = 'hsl(229, 10%, 57%)'
+    lista3.style.color = 'hsl(229, 10%, 57%)'
 
-    lista1.style.background = 'hsl(236, 72%, 79%)'
-    lista2.style.background = 'hsl(236, 72%, 79%)'
-    lista3.style.background = 'hsl(236, 72%, 79%)'
+    lista1.style.background = 'white'
+    lista2.style.background = 'white'
+    lista3.style.background = 'white'
 
-    lista1.style.fontWeight = 'unset'
-    lista2.style.fontWeight = 'unset'
-    lista3.style.fontWeight = 'unset'
+    lista1.style.fontWeight = 'bold'
+    lista2.style.fontWeight = 'bold'
+    lista3.style.fontWeight = 'bold'
 
-    preços.style.color = 'white'
-    rs.style.color = 'white'
-    plano.style.color = 'white'
+    preços.style.color = 'hsl(232, 13%, 33%)'
+    preços.style.fontSize = '45pt'
+    rs.style.color = 'hsl(232, 13%, 33%)'
+   // rs.style.left = '0%'
+    plano.style.color = 'hsl(232, 6%, 49%)'
+    plano.style.fontSize = '15pt'
+    card1.style.background = 'white'
 
-    card1.style.background = 'hsl(236, 72%, 79%)'
-    link.style.background = 'white'
-    linka.style.color = 'hsl(236, 72%, 79%)'
+    link.style.background = 'hsl(236, 72%, 79%)'
+    linka.style.color = 'white'
+ }
+function voltaCor2() {
+
+    var plano = document.getElementById('pl2')
+    var rs = document.getElementById('rs2')
+    var preços = document.getElementById('preços2')
+    var grupoL = document.getElementById('grupoLista2')
+    var lista1 = document.getElementById('lista4')
+    var lista2 = document.getElementById('lista5')
+    var lista3 = document.getElementById('lista6')
+    var link = document.getElementById('link2')
+    var linka = document.getElementById('linka2')
+
+    lista1.style.borderColor = 'gray'
+    lista2.style.borderColor = 'gray'
+    grupoL.style.borderColor = 'gray'
+
+    lista1.style.color = 'hsl(229, 10%, 57%)'
+    lista2.style.color = 'hsl(229, 10%, 57%)'
+    lista3.style.color = 'hsl(229, 10%, 57%)'
+
+    lista1.style.background = 'white'
+    lista2.style.background = 'white'
+    lista3.style.background = 'white'
+
+    lista1.style.fontWeight = 'bold'
+    lista2.style.fontWeight = 'bold'
+    lista3.style.fontWeight = 'bold'
+
+    preços.style.color = 'hsl(232, 13%, 33%)'
+    preços.style.fontSize = '45pt'
+    rs.style.color = 'hsl(232, 13%, 33%)'
+   // rs.style.left = '0%'
+    plano.style.color = 'hsl(232, 6%, 49%)'
+    plano.style.fontSize = '15pt'
+    card2.style.background = 'white'
+
+    link.style.background = 'hsl(236, 72%, 79%)'
+    linka.style.color = 'white'
+  }
+
+function voltaCor3() {
+      var plano = document.getElementById('pl3')
+      var rs = document.getElementById('rs3')
+      var preços = document.getElementById('preços3')
+      var grupoL = document.getElementById('grupoLista3')
+      var lista1 = document.getElementById('lista7')
+      var lista2 = document.getElementById('lista8')
+      var lista3 = document.getElementById('lista9')
+      var link = document.getElementById('link3')
+      var linka = document.getElementById('linka3')
+
+      lista1.style.borderColor = 'gray'
+      lista2.style.borderColor = 'gray'
+      grupoL.style.borderColor = 'gray'
+
+      lista1.style.color = 'hsl(229, 10%, 57%)'
+      lista2.style.color = 'hsl(229, 10%, 57%)'
+      lista3.style.color = 'hsl(229, 10%, 57%)'
+
+      lista1.style.background = 'white'
+      lista2.style.background = 'white'
+      lista3.style.background = 'white'
+
+      lista1.style.fontWeight = 'bold'
+      lista2.style.fontWeight = 'bold'
+      lista3.style.fontWeight = 'bold'
+
+      preços.style.color = 'hsl(232, 13%, 33%)'
+      preços.style.fontSize = '45pt'
+      rs.style.color = 'hsl(232, 13%, 33%)'
+      //rs.style.left = '0%'
+    plano.style.color = 'hsl(232, 6%, 49%)'
+    plano.style.fontSize = '15pt'
+      card3.style.background = 'white'
+
+      link.style.background = 'hsl(236, 72%, 79%)'
+      linka.style.color = 'white'
+
+    }
+card1.addEventListener('click', mudaCor1)
+card2.addEventListener('click', mudaCor2)
+card3.addEventListener('click', mudaCor3)
+
+card2.addEventListener('click', voltaCor1)
+card3.addEventListener('click', voltaCor1)
+
+card1.addEventListener('click', voltaCor2)
+card3.addEventListener('click', voltaCor2)
+
+card1.addEventListener('click', voltaCor3)
+card2.addEventListener('click', voltaCor3)
+
+function mensal() {
+    document.getElementById('preços').innerHTML = '<b>19,99</b>'
+    document.getElementById('preços2').innerHTML = '<b>24,99</b>'
+    document.getElementById('preços3').innerHTML = '<b>39,99</b>'
+    botao2.addEventListener('click', anual)
+    validar = 1
+  }
+function anual() {
+   
+    document.getElementById('preços').innerHTML = '<b>199,99</b>'
+    document.getElementById('preços2').innerHTML = '<b>249,99</b>'
+    document.getElementById('preços3').innerHTML = '<b>399,99</b>'
+    validar = 0
+    botao2.addEventListener('click', mensal)
+    
 }
+
+document.getElementById('preços').innerHTML = '<b>19,99</b>'
+document.getElementById('preços2').innerHTML = '<b>24,99</b>'
+document.getElementById('preços3').innerHTML = '<b>39,99</b>'
+
+
+setInterval(
+botao2.addEventListener('click', anual)
+) 
+    
